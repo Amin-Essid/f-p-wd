c6849b3e80fd14cb75f0df4ae0f4601ee97426f2
unknown <essidmohamedamin@gmail.com>
2021-05-04 15:19:40 +0200

    optimize it for mobile devices

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
-import UserProvider from "../context/userContext";
 import "../css/main.css";
 
 // Custom App to wrap it with context provider
 export default function App({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <div className="container">
-        <Component {...pageProps} />
-      </div>
-    </UserProvider>
+    <div className="container">
+      <Component {...pageProps} />
+    </div>
   );
 }
