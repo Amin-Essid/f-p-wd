9a72ba915cecdb77e28cda1b99bb6404804a624e
unknown <essidmohamedamin@gmail.com>
2021-05-11 13:38:44 +0100

    build image gallery

--- components/VehicleDetails.js
+++ components/VehicleDetails.js
@@ -1,16 +1,31 @@
-import Banner from "./Banner";
-// import rectangle from "../images/header_rectangle.png";
-// import gladiator from "../images/gladiatorbasica_thumbnail.png";
-import Image from "next/image";
-import Nav from "./Nav";
+const calcPay = (p, pago, r, t) => {
+  let n = 12;
+  p = p - pago;
+  console.log(p, pago, r, t);
+  let first = ((r / n) * (1 + r / n) ** n) ** t;
+  console.log(first);
+  // console.log(p, pago, r, t);
+  let payment =
+    (p * ((r / n) * (1 + r / n) ** n) ** t) / (1 + ((r / n) ** n) ** t - 1);
+  return payment;
+};
 
-export default function VehicleDetails({ info, year, age }) {
+export default function VehicleDetails({
+  info,
+  year,
+  age,
+  setPago,
+  setAnos,
+  setInteris,
+  monthly,
+}) {
   let a = "";
   if (age === true) {
     a = "Nuevo";
   } else {
     a = "usó";
   }
+  console.log(calcPay(110000, 10000, 0.06, 30));
   return (
     <>
       <div className="vp_dash">
@@ -44,6 +59,7 @@ export default function VehicleDetails({ info, year, age }) {
                   marginLeft: "10px",
                   height: "30px",
                 }}
+                onChange={(e) => setInteris(e.target.value)}
               ></input>
             </div>
             <div
@@ -63,6 +79,27 @@ export default function VehicleDetails({ info, year, age }) {
                   marginLeft: "20px",
                   height: "30px",
                 }}
+                onChange={(e) => setAnos(e.target.value)}
+              ></input>
+            </div>
+            <div
+              style={{
+                textAlign: "center",
+                margin: "10px 0 20px -30px",
+                fontSize: "20px",
+              }}
+            >
+              <label htmlFor="pago">Pago Inicial</label>
+              <input
+                name="pago"
+                id="pago"
+                defaultValue="0"
+                style={{
+                  textAlign: "center",
+                  marginLeft: "10px",
+                  height: "30px",
+                }}
+                onChange={(e) => setPago(e.target.value)}
               ></input>
             </div>
           </div>
@@ -77,7 +114,7 @@ export default function VehicleDetails({ info, year, age }) {
                 marginTop: "10px",
               }}
             >
-              399 euros
+              {`${monthly} euros`}
             </div>
           </div>
         </div>
--- pages/vehicles/[vehicle].js
+++ pages/vehicles/[vehicle].js
@@ -1,16 +1,23 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
 import Header from "../../components/Header";
 import { getVehicle } from "../../utils/getVehicle";
 import Footer from "../../components/Footer";
 import Image from "next/image";
 import VehicleDetailsfrom from "../../components/VehicleDetails";
 import Button from "@material-ui/core/Button";
+import ImageGallery from "react-image-gallery";
 
 export default function SSRPage({ data }) {
   function numberWithCommas(x) {
     return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
   }
+
+  const [interis, setInteris] = useState("5%");
+  const [anos, setAnos] = useState("10");
+  const [pago, setPago] = useState("0");
+  const [monthly, setMonthly] = useState(0);
   const { vehicle } = data;
   console.log(vehicle);
   return (
@@ -22,15 +29,6 @@ export default function SSRPage({ data }) {
 
       <main>
         <Header banner={vehicle.mainImage} />
-        <div className="vp_mobile_image">
-          <div style={{}}>
-            <img
-              id="banner"
-              src={vehicle.mainImage}
-              style={{ width: "100%", height: "100%" }}
-            />
-          </div>
-        </div>
         <div className="vp_title">
           <div className="vp_title_name">{vehicle.name}</div>
           <div className="vp_title_price">{`£${numberWithCommas(
@@ -40,11 +38,11 @@ export default function SSRPage({ data }) {
             <a
               href="https://www.facebook.com/"
               target="_blank"
-              style={{ cursor: "pointer", height: "50px" }}
+              style={{ cursor: "pointer", height: "40px" }}
             >
               <Image
-                width={50}
-                height={50}
+                width={40}
+                height={40}
                 className="rectangle"
                 src="/facebook.png"
               />
@@ -52,21 +50,27 @@ export default function SSRPage({ data }) {
             <a
               href="https://www.whatsapp.com/"
               target="_blank"
-              style={{ cursor: "pointer", height: "50px" }}
+              style={{ cursor: "pointer", height: "40px" }}
             >
               <Image
-                width={50}
-                height={50}
+                width={40}
+                height={40}
                 className="rectangle"
                 src="/whatsapp.png"
               />
             </a>
           </div>
         </div>
+
+        <ImageGallery items={vehicle.images} />
         <VehicleDetailsfrom
           year={vehicle.year}
           age={vehicle.new}
           info={vehicle.carateristicas}
+          setInteris={setInteris}
+          setAnos={setAnos}
+          setPago={setPago}
+          monthly={monthly}
         />
         <div className="vp_button">
           <Button
